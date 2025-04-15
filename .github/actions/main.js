ab650ec0a43a9e9cd6d3d68d838449b3ed53eadb
JoshMith <150962666+JoshMith@users.noreply.github.com>
2025-04-15 23:47:43 +0300

    Changed the main.js into an async function

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,28 +1,23 @@
-
 const core = require('@actions/core') // Helps with inputs and outputs
-const exec = require('@actions/exec') // Helps with with executing commands for uploading to S3
+const exec = require('@actions/exec') // Helps with executing commands for uploading to S3
 
+async function run() {
+    try {
+        // Get inputs
+        const bucket = core.getInput('bucket', { required: true })
+        const bucketRegion = core.getInput('bucket-region', { required: true })
+        const distFolder = core.getInput('dist-folder', { required: true })
 
-function run(){
-    // Get inputs
-    const bucket = core.getInput('bucket', { required: true })
-    const bucketRegion = core.getInput('bucket-region', { required: true })
-    const distFolder = core.getInput('dist-folder', { required: true })
+        // Upload files to S3
+        const s3URI = `s3://${bucket}`
+        await exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
 
-    // Upload files to S3
-    const s3URI = `s3://${bucket}`
-    exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
-    // `aws s3 sync folder1 s3://skillmatchjoshmith --region us-east-1`
-
-
-    // Get URI
-    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
-    // http://skillmatchjoshmith.s3-website.eu-north-1.amazonaws.com
-    core. setOutput('website-url', websiteUrl)
-
+        // Set output for deployment URL
+        const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+        core.setOutput('website-url', websiteUrl)
+    } catch (error) {
+        core.setFailed(error.message)
+    }
 }
 
 run()
-.catch((error) => {
-    core.setFailed(error.message)
-})
